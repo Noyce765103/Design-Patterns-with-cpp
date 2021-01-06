0262e04e74fafbf8a29dc635b9278c38cbb16256
Noyce.homecomputer1 <836641336@qq.com>
2021-01-06 19:06:50 +0800

    add adapter pattern

--- Design Patterns.cpp
+++ Design Patterns.cpp
@@ -6,6 +6,10 @@
 #include "Observerable.h"
 using namespace std;
 
+#define ADAPTER
+
+//------------------------------------观察者模式------------------------------------------------------------------------------------
+#ifdef OBSERVER
 class User1 :public Observer
 {
 	virtual void Update(void* pArg)
@@ -30,24 +34,95 @@ public:
 		SetChange("News:" + str);
 	}
 };
+#endif // OBSERVER
+//---------------------------------------------------------------------------------------------------------------------------------
+
+#ifdef ADAPTER
+
+class LegacyRectangle
+{
+public:
+	LegacyRectangle(double x1, double y1, double x2, double y2)
+	{
+		_x1 = x1;
+		_y1 = y1;
+		_x2 = x2;
+		_y2 = y2;
+		
+		
+	}
+	void LegacyDraw()
+	{
+		cout << "LegacyRectangle::LegacyDraw()" << endl;
+	}
+
+private:
+	double _x1;
+	double _y1;
+	double _x2;
+	double _y2;
+};
+
+class Rectangle
+{
+public:
+	virtual void Draw(string str) = 0;
+};
+
+//第一种适配的方式：使用多重继承
+class RectangleAdapter : public Rectangle, public LegacyRectangle
+{
+public:
+	RectangleAdapter(double x, double y, double w, double h) :LegacyRectangle(x, y, x + w, y + h)
+	{
+		cout << "RectangleAdapter(int x, int y, int w, int h)" << endl;
+	}
+	virtual void Draw(string str)
+	{
+		cout << "RectangleAdapter::Draw()" << endl;
+		LegacyDraw();
+	}
+
+};
+
+//组合方式的Adapter
+class RectangleAdapter2 :public Rectangle
+{
+public:
+	RectangleAdapter2(double x, double y, double w, double h) :_IRect(x, y, x + w, y + h)
+	{
+		cout << "RectangleAdapter(int x, int y, int w, int h)" << endl;
+	}
+	virtual void Draw(string str)
+	{
+		cout << "RectangleAdapter::Draw()" << endl;
+		_IRect.LegacyDraw();
+	}
+private:
+	LegacyRectangle _IRect;
+};
+
+#endif // ADAPTER
 
 int main()
 {	
 	//Singleton::getInstance()->DoSomething();
 	//MagicSingleton::getInstance().DoSomething();
+#ifdef OBSERVER
+#endif // OBSERVER
 
-	User1 u1;
-	User2 u2;
+#ifdef ADAPTER
+	double x = 20.0, y = 50.0, w = 300.0, h = 200.0;
+	RectangleAdapter ra(x, y, w, h);
+	Rectangle* pR = &ra;
+	pR->Draw("Testing Adapter");
+	cout << endl;
+	RectangleAdapter2 rb(x, y, w, h);
+	Rectangle* pR2 = &rb;
+	pR2->Draw("Testing Adapter");
 
-	News n1;
-	n1.GetSomeNews("T0");
+#endif // ADAPTER
 
-	n1.Attach(&u1);
-	n1.Attach(&u2);
-	n1.GetSomeNews("T1");
-
-	n1.detach(&u2);
-	n1.GetSomeNews("T2");
 
 	return 0;
 }
